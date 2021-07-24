e4cc174b95875eecbae4164c89e996ee6b32e6b8
YuZhang-SMU <87842101+YuZhang-SMU@users.noreply.github.com>
2021-07-24 22:26:25 +0800

    Add files via upload

---
+++ RaSR Code/Feature_extraction.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import mahotas
+import os, time, random
+
+data_path = 'D:/KIRC_Data/KIRC_his/KIRC_his_all' # File location of path after unified magnification and histogram matching
+patient_list1 = os.listdir(data_path)
+patient_list2 = [i[0:12] for i in patient_list1]
+
+for m in range(len(patient_list1)):
+    image_list = os.listdir(os.path.join(data_path, patient_list1[m]))
+    print('patient %d' % m)
+    for times in range(int(len(image_list))):
+        image_path = os.path.join(data_path,patient_list1[m], image_list[times])
+        image = mpimg.imread(image_path)
+        image_uint8 = image.astype('uint8')
+        feature = mahotas.features.pftas(image_uint8)
+        feature = feature[np.newaxis,:]
+        if times == 0:
+            one_patient = feature
+        else:
+            one_patient = np.vstack((one_patient,feature))
+
+    one_patient1 = np.mean(one_patient,axis=0)
+    if m == 0:
+        feature_matrix = one_patient1
+    else:
+        feature_matrix = np.vstack((feature_matrix,one_patient1))
+
+import pandas as pd
+output = pd.DataFrame(feature_matrix)
+output.to_csv('D:/KIRC_Data/KIRC_his/KIRC_pftas.csv') # File to save pftas features
+name = pd.DataFrame(patient_list2)
+name.to_csv('D:/KIRC_Data/KIRC_his/KIRC_Pftas_name.csv') # File to Save the patient number corresponding to the pftas features file
+time_end=time.time()
