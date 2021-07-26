7f047c2876907dd368a1633da1a91b7f206b5fa9
YuZhang-SMU <87842101+YuZhang-SMU@users.noreply.github.com>
2021-07-26 21:35:06 +0800

    Update solve_beta.m

--- RaSR Code/solve_beta.m
+++ RaSR Code/solve_beta.m
@@ -1,9 +1,9 @@
 function new_beta = solve_beta(Status,M,c3,beta0)
-    function B = func(beta) %要求的变量是beta
+    function B = func(beta)
         [m,k] = size(M);
         E =Status;
         rank_triangle = tril(ones(m,m-1),-1);
-        A = repmat(exp(M*beta),1,m-1).*rank_triangle;  %使用三角形矩阵来实现累加
+        A = repmat(exp(M*beta),1,m-1).*rank_triangle;
         B = -c3*sum(repmat(E(1:end-1)',k,1).*(M(1:end-1,:)' - ((A')*M)'./(repmat(sum((A),1),k,1))),2);
     end
 new_beta = fsolve(@func,beta0,options);
